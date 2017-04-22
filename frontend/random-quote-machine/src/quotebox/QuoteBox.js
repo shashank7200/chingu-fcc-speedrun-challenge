8fc24fe06b6a2fc48991d0e92750119bdc8e09c8
shashank7200 <shashank7200@gmail.com>
2017-04-23 01:02:18 +0530

    added logic for state transfer and finished this project :thumbsup:

--- frontend/random-quote-machine/src/quotebox/QuoteBox.js
+++ frontend/random-quote-machine/src/quotebox/QuoteBox.js
@@ -10,36 +10,61 @@ class QuoteBox extends Component{
     super(props);
 
     this.state = {
-        color : "#8a2be2"
+        color : "#8a2be2",
+        quote : "The world is the great gymnasium where we come to make ourselves strong.",
+        author: "Swami Vivekananda"
     };
-    console.log(colors[3]);
     this.changeQuote = this.changeQuote.bind(this);
   }
 
 changeQuote(){
-    let colorsIndex = Math.floor( Math.random () * (113 - 1 + 1)) + 1;
-    let quotesIndex = Math.floor( Math.random () * (100 - 1 + 1)) + 1;
+    let colorsIndex = Math.floor( Math.random () * (92 - 1 + 1)) + 1;
+    let quotesIndex = Math.floor( Math.random () * (85 - 1 + 1)) + 1;
+    this.setState({ quote : quotes[quotesIndex][0], author : quotes[quotesIndex][1] });
     $('.App').css("background-color",colors[colorsIndex]);
-    $('.quote').html(quotes[quotesIndex][0]);
-    $('.author').html(quotes[quotesIndex][1]);
+    $('.quote').css("color",colors[colorsIndex]);
+    // $('.quote').html('<i class="fa fa-quote-left" aria-hidden="true"></i>'+quotes[quotesIndex][0]);
+    $('.author').css("color",colors[colorsIndex]);
+    // $('.author').html('<i class="fa fa-hashtag" aria-hidden="true"></i>'+quotes[quotesIndex][1]);
+    $('.fa-twitter').css("background-color",colors[colorsIndex]);
+    $('.nextButton').css("background-color",colors[colorsIndex]);
+    $(".quote").animate({ opacity: 0 }, 500,function() { $(this).animate({ opacity: 1 }, 500);
+    $('.quote').html('<i class="fa fa-quote-left" aria-hidden="true"></i>'+quotes[quotesIndex][0]); });
+    $(".author").animate({ opacity: 0 }, 500,function() { $(this).animate({ opacity: 1 }, 500);
+    $('.author').html('<i class="fa fa-hashtag" aria-hidden="true"></i>'+quotes[quotesIndex][1]); });
 }
 
 render(){
-    const submit = "Next";
+    const submit = "New Quote";
+    const linkStyle ={
+      textDecoration : "none",
+      color: "#fff",
+      cursor: "pointer"
+    };
+    let currentQuote=this.state.quote ;
+    let currentAuthor=this.state.author;
+    let tweetLink = 'https://twitter.com/intent/tweet?hashtags=quotes&text="' + currentQuote + '" ' + currentAuthor;
+    console.log(tweetLink);
     return(
         <div className="QuoteContainer">
            <div className="QuoteBox">
-              <blockquote className="quote">Contains Quote</blockquote>
-              <p className="author">Prints Author name</p>
-              <div className="footer"><a href="#" className="tweet">
-                <i className="fa fa-twitter" aria-hidden="true"></i>
+              <blockquote className="quote color-transition"><i className="fa fa-quote-left" aria-hidden="true"></i>{this.state.quote}</blockquote>
+            <p className="author color-transition"><i className="fa fa-hashtag" aria-hidden="true"></i>{this.state.author}</p>
+              <div className="footer"><a href={tweetLink} className="tweet" title="Let's tweet this!">
+                <i className="fa fa-twitter color-transition" aria-hidden="true"></i>
               </a>
-              <button className="nextButton" onClick={this.changeQuote}>{submit}</button>
+              <button className="nextButton color-transition" onClick={this.changeQuote}>{submit}</button>
+                <div className="copy">
+     				   	   <i className="fa fa-code"></i>&nbsp; with &nbsp;<i className="fa fa-heart"></i>&nbsp; &amp; &nbsp;<i className="devicon-react-original colored"></i> by &nbsp;
+                     <a href="https://shashank7200.github.io" target="_blank" style={linkStyle}>Shashank Shekhar</a>
+     				   </div>
               </div>
+
            </div>
+
         </div>
     );
   }
 }
 
-export default QuoteBox
+export default QuoteBox;
