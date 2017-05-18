f049ac2748bfea8cd5fbdcac21cd3e578449c52d
shashank7200 <shashank7200@gmail.com>
2017-05-18 15:31:04 +0530

    made  changes to channels links

--- frontend/twitch-client/src/helpers/Channels.js
+++ frontend/twitch-client/src/helpers/Channels.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react'
 
 class Channels extends Component{
     render(){
-
+        let { name, logoUrl } = this.props
         return(
             <div className="channel-list-container">
                 <div className="channel-desc">
-                    <h3 className="channel-name">Free Code Camp</h3>
-                    <img src="https://static-cdn.jtvnw.net/jtv_user_pictures/freecodecamp-profile_image-d9514f2df0962329-300x300.png" alt="Channel Logo" className="channel-logo"/>
-                    <h3 className="channel-tv-description">#Javascript camp</h3>
+                    <h3 className="channel-name">{ name }</h3>
+                    <img src={ logoUrl } alt="Channel Logo" className="channel-logo"/>
+                    <h3 className="channel-tv-description">Hang out</h3>
                 </div>
                 <div className="channel-status">
                     <img src="https://media.giphy.com/media/EXHXTLY7ntczK/giphy.gif" alt="Channel Live Preview" className="channel-live-preiew"/>
